5fb40ff6f91dbf02682bfb7f74bb35911a01ca4a
eunidad <eunidad@ryerson.ca>
2026-07-26 11:58:02 -0400

    added docstrings for aggregatoror

--- services/downloader/src/aggregator.py
+++ services/downloader/src/aggregator.py
@@ -6,7 +6,20 @@ class HistoricalAggregator:
     _SUPPORTED_INTERVALS = {'1d':None, '1wk':"W-FRI", '2wk':"2W-FRI", '1mo':'ME', '2mo':'2ME', '3mo':'3ME'}
     
     @staticmethod
-    def _reformat_dates(df:pd.DataFrame, interval:str='1mo'):
+    def _reformat_dates(df:pd.DataFrame, interval:str='1mo') -> pd.DataFrame:
+        """
+        helper method to make dates more intuitive.
+
+        Args:
+            df (pd.DataFrame): the dataframe to be reformatted
+            interval (str): The interval to be reported. one of product(['1', '2', '3', ...], ['d', 'wk', 'mo', 'y'])
+                            e.g.: "3d" -> (jan 1, jan 2, jan 3), (jan 4, jan 5, jan 6), ... -> jan 3, jan 6, ...
+                                  "1mo" -> (jan 1, jan 2, jan 3...), (feb 1, feb 2, ...)... -> jan 31, feb 28, ...
+                                  "2mo" -> (jan 1, jan2, ... feb 1, feb 2...), (mar 1, ..., apr 1, ...), ... -> feb 28, apr 30
+        
+        Returns:
+            pd.DataFrame: The formatted dataframe
+        """
         dates = df.index
         if 'mo' in interval:
             dates = dates.strftime('%Y-%m')
@@ -17,6 +30,19 @@ class HistoricalAggregator:
         return df
 
     def aggregate(self, df:pd.DataFrame, interval:str='1mo') -> pd.DataFrame:
+        """
+        OHLC aggregator for requested interval; turning the interval into a "day"
+
+        Args:
+            df (pd.DataFrame): the data to be aggregated
+            interval (str):  The interval to be reported. one of product(['1', '2', '3', ...], ['d', 'wk', 'mo', 'y'])
+                            e.g.: "3d" -> (jan 1, jan 2, jan 3), (jan 4, jan 5, jan 6), ... -> jan 3, jan 6, ...
+                                  "1mo" -> (jan 1, jan 2, jan 3...), (feb 1, feb 2, ...)... -> jan 31, feb 28, ...
+                                  "2mo" -> (jan 1, jan2, ... feb 1, feb 2...), (mar 1, ..., apr 1, ...), ... -> feb 28, apr 30
+        
+        Returns:
+            pd.DataFrame: the new dataframe of aggregated data
+        """
         if df.empty:
             raise PriceAggregationError('Dataframe is empty')
         if interval not in self._SUPPORTED_INTERVALS:
